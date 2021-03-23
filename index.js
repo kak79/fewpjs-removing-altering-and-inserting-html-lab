5317751796f68b2858c1d70dfa4ef35d1ba2687c
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-03-23 12:41:51 -0500

    commit tree 4e08dc716c7b427d2deaab70582ba2c2191fce55

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your code here!
+let element1 = document.createElement('div') 
+
+document.body.appendChild(element1) 
+
+let ul = document.createElement('ul')
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+
+element1.appendChild(ul) 
+
+let p = document.createElement('p')
+
+p.id = 'greeting'
+
+element1.appendChild(p)
+
+let element2 = document.querySelector("p#greeting");
+element2.innerHTML = 'Hello, DOM!' 
+
+let div = document.createElement('div')
+
+div.id = 'header'
+
+element1.appendChild(div)
+
+let header = document.getElementById('header');
+header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>"; 
+
+header.style.bold
+
+let main = document.getElementById('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "<h1> Kim is the champion </h1>"
+
